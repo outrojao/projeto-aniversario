266323ff5605ca551b68f74e3a77864c38b60bf7
Sants16 <jvsantosrocha980@gmail.com>
2022-06-28 17:00:02 -0300

    28/06

--- poemas.js
+++ poemas.js
@@ -1,4 +1,4 @@
-let botão = document.querySelector('input[type=button]');
+let botão = document.querySelector('#gerarPoema');
 let poemas = document.querySelector('#poema')
 let frases = [
     'Gata eu tô perdido, eu queria saber qual é o ônibus que eu pego para ir ao encontro da sua boca',
@@ -11,7 +11,8 @@ let frases = [
     'Não sou Shakira mas eu vou deixar você louca louca louca',
     'Você é o sol e eu sou a lua, num eclipse de amor minha boca beija a sua',
     'Tô pensando numa coisa que na verdade eu não sei mas se você vier aqui eu te conto',
-    'Me chama de cheque especial e me segura por alguns dias'
+    'Me chama de cheque especial e me segura por alguns dias',
+    ''
 ]
 
 botão.addEventListener('click', gerarPoema);
